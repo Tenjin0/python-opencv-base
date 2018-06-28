8497cdb9a3fc3eb67b036a65cd5eca0a0330bc66
Patrice <ppetit@wynd.eu>
2018-06-28 10:09:39 +0200

    Core: imread

--- 2_images_shape.py
+++ 2_images_shape.py
@@ -4,16 +4,16 @@ import numpy as numpy
 
 img = numpy.zeros((3, 3), dtype=numpy.uint8)
 shape = img.shape
-print shape, len(shape)
-img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+print img
+img2 = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
 shape = img.shape
-print shape, len(shape)
+print img2
 
 cv2.imshow("display", img)
+cv2.imshow("display2", img2)
 
 while True:
-    k = cv2.waitKey()
-    if cv2.waitKey(113):
+    if cv2.waitKey(0) & 0xFF == ord('q'):
         break
 
 cv2.destroyAllWindows()
---
+++ 3_images_read.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as numpy
+
+
+original_image = cv2.imread("images/peppers_color.tif")
+# img2 = cv2.imread("images/peppers_color.tif", cv2.IMREAD_GRAYSCALE)
+# img3 = cv2.imread("images/peppers_color.tif", cv2.IMREAD_ANYCOLOR)
+# img4 = cv2.imread("images/peppers_color.tif", cv2.IMREAD_ANYDEPTH)
+# img5 = cv2.imread("images/peppers_color.tif", cv2.IMREAD_COLOR)
+# img6 = cv2.imread("images/peppers_color.tif", cv2.IMREAD_LOAD_GDAL)
+# img7 = cv2.imread("images/peppers_color.tif", cv2.IMREAD_UNCHANGED)
+
+cv2.imshow("display", original_image)
+# cv2.imshow("display2", img2)
+# cv2.imshow("display3", img3)
+# cv2.imshow("display4", img4)
+# cv2.imshow("display5", img5)
+# cv2.imshow("display6", img6)
+# cv2.imshow("display7", img7)
+
+while True:
+    if cv2.waitKey(0) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
