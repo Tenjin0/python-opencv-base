d0a9897ec48da2f42ee4408429310e73908668c6
Patrice <ppetit@wynd.eu>
2018-09-18 10:35:46 +0200

    Core: add face detectection

--- introduction/22_face_detection_image.py
+++ introduction/22_face_detection_image.py
@@ -5,7 +5,6 @@ import os
 filename = os.path.join(os.getcwd(),
                         "images", "people.jpg")
 
-print filename
 gray_faces = []
 
 
@@ -13,27 +12,34 @@ def detect(filename):
     global gray_faces
 
     face_cascade = cv2.CascadeClassifier(
-        './cascades/haarcascade_frontalface_default.xml')
+        './cascades/haarcascade_frontalface_alt2.xml')
 
     eye_cascade = cv2.CascadeClassifier(
-        './cascades/eye_cascade.xml')
+        './cascades/haarcascade_eye.xml')
+
+    smile_cascade = cv2.CascadeClassifier(
+        './cascades/haarcascade_smile.xml')
 
     img = cv2.imread(filename)
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # print gray
 
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 6)
     for (x, y, w, h) in faces:
         # print x, y, w, h
         img = cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
         face_gray = gray[y:y+h, x:x+w]
         if len(face_gray) > 0:
-            print(face_gray)
             gray_faces.append(face_gray)
-            eyes = eye_cascade.detectMultiScale(face_gray)
-            # for (ex, ey, ew, eh) in eyes:
-            #     cv2.rectangle(face_gray, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+            eyes = eye_cascade.detectMultiScale(face_gray,  1.1, 1)
+            for (ex, ey, ew, eh) in eyes:
+                cv2.rectangle(img, (x + ex, y + ey),
+                              (x + ex+ew, y + ey+eh), (0, 255, 0), 2)
+            smiles = smile_cascade.detectMultiScale(face_gray, 2.0, 5)
+            for (ex, ey, ew, eh) in smiles:
+                cv2.rectangle(img, (x + ex, y + ey),
+                              (x + ex+ew, y + ey+eh), (0, 0, 255), 2)
 
     cv2.namedWindow("Face detected")
     cv2.imshow("Face detected", img)
