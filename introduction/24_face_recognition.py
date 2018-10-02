faec15fa5dc4b0a393703bce56d0b73a1a442f13
Patrice <ppetit@wynd.eu>
2018-10-02 10:36:52 +0200

    Core: add face recognition

--- introduction/24_face_recognition.py
+++ introduction/24_face_recognition.py
@@ -98,6 +98,43 @@ def read_images(path, sz=None):
 
 if __name__ == "__main__":
 
-    # storeFolder = createDir()
-    # generate(storeFolder)
-    read_images("data")
+    if len(sys.argv) < 2:
+        print "USAGE: facerec_demo.py </path/to/images> "
+        sys.exit()
+
+    [X, y] = read_images(sys.argv[1])
+    y = np.asarray(y, dtype=np.int32)
+
+    if len(sys.argv) == 3:
+        out_dir = sys.argv[2]
+
+    model = cv2.face.EigenFaceRecognizer_create()
+    model.train(np.asarray(X), np.asarray(y))
+    camera = cv2.VideoCapture(0)
+    camera.release()
+
+    face_cascade = cv2.CascadeClassifier(
+        "./cascades/haarcascade_frontalface_default.xml")
+
+    while(True):
+        read, img = camera.read()
+        faces = face_cascade.detectMultiScale(img, 1.3, 5)
+        for (x, y, w, h) in faces:
+            img = cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            roi = gray[x:x+w, y:y+h]
+            # try:
+            #     roi = cv2.resize(roi, (200, 200),
+            #                      interpolation=cv2.INTER_LINEAR)
+            #     params = model.predict(roi)
+            #     print "Label: %s, Confidence: %.2f" % (params[0], params[1])
+            #     cv2.putText(img, "toto", (x, y - 20),
+            #                 cv2.FONT_HERSHEY_SIMPLEX, 1, 255, 2)
+            # except:
+            #     continue
+            cv2.imshow("camera", img)
+
+            if cv2.waitKey(1000 / 12) & 0xff == ord("q"):
+                break
+    cv2.destroyAllWindows()
+    camera.close()
