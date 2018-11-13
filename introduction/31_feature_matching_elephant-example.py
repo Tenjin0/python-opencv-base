4f158cac4c74d8a67d52f0f28e28d009955e82da
Patrice <ppetit@wynd.eu>
2018-11-13 10:37:32 +0100

    Core: detect future elephant [Daily WIP]

--- introduction/31_feature_matching_elephant-example.py
+++ introduction/31_feature_matching_elephant-example.py
@@ -1,6 +1,11 @@
 import sys
 import cv2
 
+PY3 = sys.version_info[0] == 3
+
+if PY3:
+    xrange = range
+
 FLANN_INDEX_KDTREE = 1
 FLANN_INDEX_LSH    = 6
 flann_params= dict(algorithm = FLANN_INDEX_LSH,
@@ -18,18 +23,34 @@ if __name__ == "__main__":
         video_src = 0
     cap = cv2.VideoCapture(video_src)
 
-    queryImage = cv2.imread('images/elephant.jpg')
+    targetImage = cv2.imread('images/elephant.jpg')
     matcher = cv2.FlannBasedMatcher(flann_params, {})  # bug : need to pass empty dict (#1329)
     imageToTest = cv2.imread('data/s2/20181112-102950-3.jpg')
-    while True:
-        ret, frame = self.cap.read()
-        if not ret:
-            break
-        frame = frame.copy()
 
-        keypoints, descrs = self.detector.detectAndCompute(frame, None)
-        if descrs is None:  # detectAndCompute returns descs=None if not keypoints found
-            descrs = []
+    targets = []
+    frame_points = []
 
-        if len(keypoints) < MIN_MATCH_COUNT:
-            tracked = []
+    detector = cv2.ORB_create( nfeatures = 1000 )
+
+    kpTarget, descTarget = detector.detectAndCompute(queryImage, None)
+
+    if descTarget is None:  # detectAndCompute returns descs=None if not keypoints found
+        descTarget = []
+    else:
+        descTarget = np.uint8(descTarget)
+
+    matches = matcher.knnMatch(descTarget, k = 2)
+    matches = [m[0] for m in matches if len(m) == 2 and m[0].distance < m[1].distance * 0.75]
+
+    # while True:
+    #     ret, frame = self.cap.read()
+    #     if not ret:
+    #         break
+    #     frame = frame.copy()
+
+    #     keypoints, descrs = self.detector.detectAndCompute(frame, None)
+    #     if descrs is None:  # detectAndCompute returns descs=None if not keypoints found
+    #         descrs = []
+
+    #     if len(keypoints) < MIN_MATCH_COUNT:
+    #         tracked = []
