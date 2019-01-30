190926575de6a7125348ef3773f8cfdc4ae78e48
Patrice <ppetit@wynd.eu>
2019-01-30 10:25:27 +0100

    Core: try detect with new image

--- introduction/34_feature_matching_multiple.py
+++ introduction/34_feature_matching_multiple.py
@@ -23,11 +23,13 @@ def tuple_float_to_int(a_tuple):
     return tuple(int(i) for i in a_tuple)
 
 
-def addImageToMatcher(trainingImage, matcher):
+def addImageToMatcher(trainingImage, matcher, KPs):
 
     trainingImage = cv2.imread(trainingImage)
     trainingCopy = cv2.cvtColor(trainingImage, cv2.COLOR_BGR2GRAY)
     trainingKPs, trainingDescs = detector.detectAndCompute(trainingCopy, None)
+
+    KPs.append(trainingKPs)
 
     if trainingDescs is None:
         trainingDescs = []
@@ -46,9 +48,9 @@ if __name__ == "__main__":
     detector = cv2.ORB_create(nfeatures=1000)
 
     matcher = cv2.FlannBasedMatcher(flann_params, {})
-
-    addImageToMatcher('images/elephant.png', matcher)
-    addImageToMatcher('images/lipton.png', matcher)
+    KPs = []
+    addImageToMatcher('images/elephant_old.png', matcher, KPs)
+    addImageToMatcher('images/lipton.jpg', matcher, KPs)
 
     targetImage = cv2.imread('data/s3/20181210-100718-3.jpg')
     targetCopy = cv2.cvtColor(targetImage, cv2.COLOR_BGR2GRAY)
@@ -62,20 +64,23 @@ if __name__ == "__main__":
 
     matches = [m[0] for m in matches if len(
         m) == 2 and m[0].distance < m[1].distance * 0.75]
+
     if len(matches) < MIN_MATCH_COUNT:
         matches = []
+
     p0 = []
     p1 = []
+
     for m in matches:
         print(m.imgIdx, m.trainIdx, m.queryIdx)
-        # p0.append([m.trainIdx].pt)
-        # p1.append(targetKPs[m.queryIdx].pt)
-    exit()
+        if (m.imgIdx == 0):
+            p0.append(KPs[m.imgIdx][m.trainIdx].pt)
+            p1.append(targetKPs[m.queryIdx].pt)
 
     p0, p1 = np.float32((p0, p1))
 
-    for (x, y) in np.int32(p0):
-        cv2.circle(trainingImage, (x, y), 10, (0, 0, 0))
+    # for (x, y) in np.int32(p0):
+    #     cv2.circle(trainingImage, (x, y), 10, (0, 0, 0))
 
     for (x, y) in np.int32(p1):
         cv2.circle(targetImage, (x, y), 10, (0, 255, 255))
@@ -85,27 +90,27 @@ if __name__ == "__main__":
 
     p0, p1 = p0[status], p1[status]
 
-    for (x, y) in np.int32(p0):
-        cv2.circle(trainingImage, (x, y), 8, (255, 255, 0))
+    # for (x, y) in np.int32(p0):
+    #     cv2.circle(trainingImage, (x, y), 8, (255, 255, 0))
     for (x, y) in np.int32(p1):
         cv2.circle(targetImage, (x, y), 8, (255, 255, 0))
 
-    width, heigth = trainingCopy.shape
+    # width, heigth = trainingCopy.shape
 
-    x0 = 0
-    y0 = 0
-    x1 = width
-    y1 = heigth
+    # x0 = 0
+    # y0 = 0
+    # x1 = width
+    # y1 = heigth
 
-    quad = np.float32([[x0, y0], [x0, y1], [x1, y1], [x1, y0]])
-    quad = quad.reshape(-1, 1, 2)
-    quad = cv2.perspectiveTransform(quad, H)
-    cv2.polylines(targetImage, [np.int32(quad)],
-                  True, (255, 255, 255), 2)
+    # quad = np.float32([[x0, y0], [x0, y1], [x1, y1], [x1, y0]])
+    # quad = quad.reshape(-1, 1, 2)
+    # quad = cv2.perspectiveTransform(quad, H)
+    # cv2.polylines(targetImage, [np.int32(quad)],
+    #               True, (255, 255, 255), 2)
     for (x, y) in np.int32(p1):
         cv2.circle(targetImage, (x, y), 2, (255, 255, 255), 2)
 
-    cv2.imshow('train', trainingImage)
+    # cv2.imshow('train', trainingImage)
     cv2.imshow('queryImage', targetImage)
     cv2.waitKey()
     cv2.destroyAllWindows()
