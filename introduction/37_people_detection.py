fb30e885b36f57611f24ea94e33736d2dbd44b60
Patrice <ppetit@wynd.eu>
2019-03-05 10:23:38 +0100

    HOG: try to detect people [WIP]

--- introduction/37_people_detection.py
+++ introduction/37_people_detection.py
@@ -3,12 +3,6 @@ import numpy as np
 import os
 
 
-def is_inside(o, i):
-    ox, oy, ow, oh = o
-    ix, iy, iw, ih = i
-    return ox > ix and oy > iy and ox + ow < ix + iw and oy + oh < iy + ih
-
-
 def draw_person(image, person):
     x, y, w, h = person
     cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 255), 2)
@@ -21,18 +15,15 @@ if __name__ == "__main__":
 
     hog = cv2.HOGDescriptor()
     hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
     found, w = hog.detectMultiScale(img)
-    found_filtered = []
+    print("people found:", len(found), len(w))
+    # found_filtered = []
+
     for ri, r in enumerate(found):
-        for qi, q in enumerate(found):
-            if ri != qi and is_inside(r, q):
-                break
-            else:
-                found_filtered.append(r)
+        draw_person(img, r)
 
-    for person in found_filtered:
-        draw_person(img, person)
+    cv2.imgshow()
 
-    cv2.imshow("people detection", img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+
+# https://www.pyimagesearch.com/2015/11/09/pedestrian-detection-opencv/
