2c7410eab6b15eb3af8f5762fbdd4f5699ab1ac3
Patrice <ppetit@wynd.eu>
2019-03-21 10:33:28 +0100

    feature/cars: with ORB [WIP]

---
+++ introduction/38_car_detection.py
@@ -0,0 +1,27 @@
+import cv2
+
+import numpy as np
+import os
+from os.path import join, normpath
+
+currentDirectory = os.path.dirname(os.path.abspath(__file__))
+
+datapath = normpath(join(currentDirectory, "..", "training", "cars"))
+# img = cv2.imread(os.path.join(currentDirectory, "..", "images", "1a_original.jpg"))
+
+detect = cv2.ORB_create()
+extract = cv2.ORB_create()
+
+flann_params = dict(algorithm=cv2.FLANN_INDEX_KDTREE, trees=5)
+flann = cv2.FlannBasedMatcher(flann_params, {})
+
+
+bow_kmeans_trainer = cv2.BOWKMeansTrainer(40)
+
+extract_bow = cv2.BOWImgDescriptorExtractor(extract, flann)
+
+
+def extract_orb(fn):
+
+    im = cv2.imread(fn, 0)
+    return extract.detectAndCompute(im, None)[1]
