4354bd0194f7de244fb643af2ff9ca91d03e4ac6
Patrice <ppetit@wynd.eu>
2019-03-25 10:38:37 +0100

    feature/detect-cars: [WIP]

--- introduction/38_car_detection.py
+++ introduction/38_car_detection.py
@@ -4,21 +4,28 @@ import numpy as np
 import os
 from os.path import join, normpath
 
+currentDirectory = os.path.dirname(os.path.abspath(__file__))
+
+datapath = normpath(join(currentDirectory, "..", "training", "cars_light"))
+
+
+def path(cls, i):
+    return "%s/%s%d.pgm" % (datapath, cls, i)
+
 
 def extract_orb(fn):
 
     im = cv2.imread(fn, 0)
-    return extract.detectAndCompute(im, None)[1]
+    print(extract.detectAndCompute(im, None))
+    return extract.detectAndCompute(im)
 
-currentDirectory = os.path.dirname(os.path.abspath(__file__))
 
-datapath = normpath(join(currentDirectory, "..", "training", "cars"))
 # img = cv2.imread(os.path.join(currentDirectory, "..", "images", "1a_original.jpg"))
 
 detect = cv2.ORB_create()
 extract = cv2.ORB_create()
 
-flann_params = dict(algorithm=cv2.FLANN_INDEX_KDTREE, trees=5)
+flann_params = dict(algorithm=1, trees=5)
 flann = cv2.FlannBasedMatcher(flann_params, {})
 
 bow_kmeans_trainer = cv2.BOWKMeansTrainer(40)
@@ -29,14 +36,14 @@ pos, neg = "pos-", "neg-"
 
 for i in range(8):
     bow_kmeans_trainer.add(extract_orb(path(pos, i)))
-    bow_kmeans_trainer.add(extract_orb(path(neg, i)))
+    # bow_kmeans_trainer.add(extract_orb(path(neg, i)))
 
-voc = bow_kmeans_trainer.cluster()
-extract_bow.setVocabulary(voc)
+# voc = bow_kmeans_trainer.cluster()
+# extract_bow.setVocabulary(voc)
 
 
-def bow_features(fn):
-    im = cv2.imread(fn, 0)
-    return extract_bow.compute(img, detect.detectAndCompute(img, None))
+# def bow_features(fn):
+#     im = cv2.imread(fn, 0)
+#     return extract_bow.compute(img, detect.detectAndCompute(img, None))
 
-traindata, trainlabels = [], []
+# traindata, trainlabels = [], []
