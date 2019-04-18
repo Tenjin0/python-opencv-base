b9732f24de688547b6d45b9d796d0ff458902874
Patrice <ppetit@wynd.eu>
2019-04-18 10:04:00 +0200

    feature/detect_cars: [WIP]

--- helpers/detect_cars.py
+++ helpers/detect_cars.py
@@ -8,7 +8,7 @@ currentDirectory = os.path.dirname(os.path.abspath(__file__))
 datapath = normpath(join(currentDirectory, "..", "training", "cars_light"))
 imagepath = normpath(join(currentDirectory, "..", "images"))
 
-SAMPLES = 100
+SAMPLES = 400
 
 pos, neg = "pos-", "neg-"
 
@@ -34,43 +34,50 @@ def train_cars(pos_glob, neg_glob):
 
     flann = get_flann_matcher()
 
-    bow_kmeans_trainer = cv2.BOWKMeansTrainer(1000)
+    bow_kmeans_trainer = cv2.BOWKMeansTrainer(500)
     extract_bow = cv2.BOWImgDescriptorExtractor(kaze, flann)
 
     traindata, trainlabels = [], []
+    exceptions = []
+    for i in range(SAMPLES):
+
+        if i <= len(pos_glob):
+            fp = pos_glob[i]
+            impos = cv2.imread(fp, 0)
+            descriptor = kaze.detectAndCompute(impos, None)[1]
+            bow_kmeans_trainer.add(descriptor)
+
+        if i <= len(neg_glob):
+            fp = neg_glob[i]
+            imneg = cv2.imread(fp, 0)
+            descriptor = kaze.detectAndCompute(imneg, None)[1]
+            bow_kmeans_trainer.add(descriptor)
+
+    voc = bow_kmeans_trainer.cluster()
+    extract_bow.setVocabulary(voc)
 
     for i in range(SAMPLES):
 
         if i <= len(pos_glob):
             fn = pos_glob[i]
-            print(i, fn)
             impos = cv2.imread(fn, 0)
-            descriptor = kaze.detectAndCompute(impos, None)[1]
-            bow_kmeans_trainer.add(descriptor)
-            bow_descriptor = extract_bow.compute(impos, detect.detect(impos, None))
+            bow_descriptor = extract_bow.compute(impos, kaze.detect(impos, None))
             traindata.extend(bow_descriptor)
             trainlabels.append(1)
 
-        # if i <= len(neg_glob):
-        #     fn = neg_glob[i]
-        #     imneg = cv2.imread(fn, 0)
-        #     bow_descriptor = extract_bow.compute(imneg, kaze.detect(imneg))
-        #     traindata.extend(bow_descriptor)
-        #     trainlabels.append(-1)
+        if i <= len(neg_glob):
+            fn = neg_glob[i]
+            imneg = cv2.imread(fn, 0)
+            bow_descriptor = extract_bow.compute(imneg, kaze.detect(imneg))
+            traindata.extend(bow_descriptor)
+            trainlabels.append(-1)
 
-            # traindata.extend(bow_features(cv2.imread(
-            #     path(pos, i), 0), extract_bow, detect))
-        # bow_kmeans_trainer.add( )
+    svm = cv2.ml.SVM_create()
+    svm.setType(cv2.ml.SVM_C_SVC)
+    svm.setGamma(1)
+    svm.setC(35)
+    svm.setKernel(cv2.ml.SVM_RBF)
+    svm.train(np.array(traindata), cv2.ml.ROW_SAMPLE, np.array(trainlabels))
 
-    # voc = bow_kmeans_trainer.cluster()
-    # extract_bow.setVocabulary(voc)
-
-    # svm = cv2.ml.SVM_create()
-    # svm.setType(cv2.ml.SVM_C_SVC)
-    # svm.setGamma(1)
-    # svm.setC(35)
-    # svm.setKernel(cv2.ml.SVM_RBF)
-    # svm.train(np.array(traindata), cv2.ml.ROW_SAMPLE, np.array(trainlabels))
-
-    # return svm, extract_bow
+    return svm, extract_bow
 
